8d754cfcc2eef33b16d25f0fa7dd26e8170b8155
Corsi Lucas <corsi.lucasgabriel.studente@itispaleocapa.it>
2023-02-24 12:31:48 +0100

    funzione di cancellazione

--- Strutture/Form1.Designer.cs
+++ Strutture/Form1.Designer.cs
@@ -33,7 +33,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.Salva = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Cancellazione = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
@@ -79,14 +79,15 @@
             this.Salva.UseVisualStyleBackColor = true;
             this.Salva.Click += new System.EventHandler(this.Salva_Click);
             // 
-            // button1
+            // Cancellazione
             // 
-            this.button1.Location = new System.Drawing.Point(266, 366);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Cancellazione.Location = new System.Drawing.Point(191, 151);
+            this.Cancellazione.Name = "Cancellazione";
+            this.Cancellazione.Size = new System.Drawing.Size(75, 23);
+            this.Cancellazione.TabIndex = 5;
+            this.Cancellazione.Text = "Cancella";
+            this.Cancellazione.UseVisualStyleBackColor = true;
+            this.Cancellazione.Click += new System.EventHandler(this.Cancellazione_Click);
             // 
             // listView1
             // 
@@ -104,7 +105,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.listView1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Cancellazione);
             this.Controls.Add(this.Salva);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -125,7 +126,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button Salva;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Cancellazione;
         private System.Windows.Forms.ListView listView1;
     }
 }
--- Strutture/Form1.cs
+++ Strutture/Form1.cs
@@ -39,6 +39,14 @@ namespace Strutture
             visualizza(P);
             
         }
+        private void Cancellazione_Click(object sender, EventArgs e)
+        {
+            Cancella(P, ref indice, Nome.Text);
+            visualizza(P);
+        }
+
+
+
         public string Pstring(Prodotto P)
         {
             return "nome: "+ P.nome+"   Prezzo: "+P.prezzo.ToString();
@@ -48,6 +56,31 @@ namespace Strutture
             listView1.Items.Clear();
             for(int i = 0; i < indice; i++)  
             listView1.Items.Add(Pstring(P[i]));
+        }
+        public void Cancella(Prodotto[] P, ref int indice, string input)
+        {
+            //ciclo per ricercare la parola nell'array
+            for (int z = 0; z < P.Length; z++)
+            {
+                //se la parola viene trovata allora
+                if (P[z].nome == input)
+                {
+                    //ciclo per sovrascrivere le parole per cancellare quella inserita
+                    while (z < indice)
+                    {
+                        P[z] = P[z + 1];
+                        z++;
+                    }
+                    //decremento dell'indice data la parola cancellata
+                    indice = indice - 1;
+                    //uscita dal ciclo 
+                    break;
+                }
+            }
+        }
+        public void Ricerca(Prodotto P)
+        {
+
         }
     }
 }
